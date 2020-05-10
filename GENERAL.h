b55b56817747c9dbe32ed1842070ed3e339eb535
Vương Chính <63763921+vuongvanchinh@users.noreply.github.com>
2020-05-10 22:34:06 +0700

    Add files via upload

--- GENERAL.h
+++ GENERAL.h
@@ -15,10 +15,10 @@ using namespace std;
 
 const int SCREEN_WIDTH = 1500;
 const int SCREEN_HEIGHT = 426;
-const int DOCAOSAN = 347;
-const int vantocnhanvat = 5;
-const int vantocnhanvatnhay = 5;
-const int vantocchimbay = 5;
+const int DOCAOSAN = 348;
+const int vantocnhanvat = 6;
+
+const int vantocchimbay = 4;
 const int vantocdan_nhanvat = 10;
 const int MANG_SONG_MAX = 10;
 const int FULLSCREEN = 3790;
@@ -27,11 +27,13 @@ const int LIMIT_FRIRST = 270;
 const int LIMIT_LAST = SCREEN_WIDTH - 600;
 const int SO_LAN_CHIM_MAX = 3;
 const int vantoc_dan_chimbay = 2;
-const int DELAY_SPRITE_NHANVAT = 4;
+const int DELAY_SPRITE_NHANVAT = 2;
 const int DELAY_SPRITE_CHIMBAY = 10;
 const int DELAY_SPRITE_QUAI_DOI = 20;
 const int DELAY_SPRITE_BULLET_QUAI_DOI = 10;
-const int SO_CHIM_BAY = 15;
+const int SO_CHIM_BAY = 20;
+const int SUC_BAT = 200;
+const int GIATOC_TRONGTRUONG = 1;
 
 enum status {
 	left_to_right,
@@ -40,8 +42,4 @@ enum status {
 	up_right_to_left
 };
   
-
-
-
-
 #endif // !GENERRAL_H
