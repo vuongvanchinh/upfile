fa7965946c081183da3592ab80d2bb0f38dc7bc8
Vương Chính <63763921+vuongvanchinh@users.noreply.github.com>
2020-05-13 20:58:55 +0700

    Update main_.cpp

--- main_.cpp
+++ main_.cpp
@@ -1162,8 +1162,8 @@ void show_score(int score)
 bool isVaCham(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2)
 {
 	// toa do x
-	bool X = ((x1 + w1 >= x2 && x1<x2+w2));
-	bool Y = ((y1 + h1 > y2 && y2 + h2 > y1));
+	bool X = ((x1 + w1 >= x2 && x1 +w1< x2 + w2)||(x2+w2>x1 && x2+w2 <x1+w1));
+	bool Y = ((y1 + h1 > y2&&  y1+h1 > y2 + h2 )|| (y2+h2>y1&& y2+h2>y1+h1));
 
 	return X && Y;
 }
